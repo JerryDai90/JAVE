96ca22bf2eb417e56804a10e02900420793b2a14
JerryDai90 <jerrydai90@gmail.com>
2018-07-10 15:01:28 +0800

    增加对 macos 支持

---
+++ src/main/java/it/sauronsoftware/jave/Test.java
@@ -0,0 +1,32 @@
+package it.sauronsoftware.jave;
+
+import java.io.File;
+
+public class Test {
+
+    public static void main(String[] args) {
+        changeToMp3("z:\\test.amr",
+                "z:\\test.mp3");
+    }
+
+
+    public static void changeToMp3(String sourcePath, String targetPath) {
+        File source = new File(sourcePath);
+        File target = new File(targetPath);
+        AudioAttributes audio = new AudioAttributes();
+        Encoder encoder = new Encoder();
+        audio.setCodec("libmp3lame");
+        EncodingAttributes attrs = new EncodingAttributes();
+        attrs.setFormat("mp3");
+        attrs.setAudioAttributes(audio);
+        try {
+            encoder.encode(source, target, attrs);
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+        } catch (InputFormatException e) {
+            e.printStackTrace();
+        } catch (EncoderException e) {
+            e.printStackTrace();
+        }
+    }
+}
